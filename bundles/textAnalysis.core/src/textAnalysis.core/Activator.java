3ea809a9d9059d9e7ce0a4d1423c96c45aa9f276
maikefer <maikefer_@gmx.de>
2020-02-24 15:58:28 +0100

    More trys to bugfix

--- bundles/textAnalysis.core/src/textAnalysis.core/Activator.java
+++ bundles/textAnalysis.core/src/textAnalysis.core/Activator.java
@@ -3,11 +3,13 @@ package textAnalysis.core;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleActivator;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+//public class Activator extends AbstractUIPlugin {
+public class Activator implements BundleActivator {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "PluginTest4"; //$NON-NLS-1$
